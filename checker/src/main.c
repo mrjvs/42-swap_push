d7ddc5d5d45e6810c65bbe777433a9682fed66ab
mrjvs <jellevs@gmail.com>
2021-04-22 15:28:47 +0200

    checker leak update

--- checker/src/main.c
+++ checker/src/main.c
@@ -5,9 +5,11 @@
 #include "push_swap_lib.h"
 #include <unistd.h>
 
-static int	send_error(void)
+static int	send_error(t_stack *a, t_stack *b)
 {
 	write(STDERR_FILENO, "Error\n", 6);
+	stack_destroy(a);
+	stack_destroy(b);
 	return (1);
 }
 
@@ -17,6 +19,8 @@ static int	send_result(t_stack *a, t_stack *b)
 		write(STDOUT_FILENO, "KO\n", 3);
 	else
 		write(STDOUT_FILENO, "OK\n", 3);
+	stack_destroy(a);
+	stack_destroy(b);
 	return (0);
 }
 
@@ -29,11 +33,11 @@ static int	runInstructions(t_stack *a, t_stack *b)
 	{
 		i = get_next_instr(instruction);
 		if (i < 0)
-			return (send_error());
+			return (1);
 		if (instruction[0] == '\0' && i == 0)
 			break ;
 		if (!run_instr(instruction, a, b))
-			return (send_error());
+			return (1);
 		if (i == 0)
 			break ;
 	}
@@ -49,15 +53,16 @@ int	main(int argc, char *argv[])
 	stack_init(&a);
 	stack_init(&b);
 	if (argc < 2)
-		return (send_error());
+		return (send_error(&a, &b));
 	i = 1;
 	while (i < argc)
 	{
-		if (!is_num(argv[1]) || !stack_push_value(&a, my_atoi(argv[i])))
-			return (send_error());
+		if (!is_num(argv[i]) || !stack_push_value(&a, my_atoi(argv[i])))
+			return (send_error(&a, &b));
 		i++;
 	}
+	// TODO check duplicate numbers in stack
 	if (runInstructions(&a, &b) != 0)
-		return (1);
+		return (send_error(&a, &b));
 	return (send_result(&a, &b));
 }
